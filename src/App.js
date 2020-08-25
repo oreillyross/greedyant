c714f911e9073ee4bf351a4f0666efdd6026de44
Ross O'Reilly <oreillyross@gmail.com>
2020-08-25 19:56:20 +0200

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import Header from './components/Header'
-import Container from './components/Container'
+/* eslint-disable react/jsx-tag-spacing */
+import React from 'react';
+import Header from './components/Header';
+import Container from './components/Container';
 
 export default function App() {
   return (
