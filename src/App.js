298c3174be39a96b48cd9d71286f930f7f1d1f5d
Faktor 10 <oreillyross@gmail.com>
2020-08-25 10:30:21 +0200

    align with codesandbox (#4)
    * merge from codesandbox (#3)
    
    * add
    
    * add
    
    * update to allow create-react-app functionality
    
    * app renders with dependencies installed
    
    * style Header
    
    * style change
    
    * refactor Header into seperate component
    
    * add react-testing library
    
    * add Container with styling
    
    * add eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import Header from './components/Header'
+import Header from './components/Header'n
+import Container from './components/Container'
 
 export default function App() {
   return (
     <div>
       <Header/>
+
+      <Container/>
+
+
 
     </div>
   );
