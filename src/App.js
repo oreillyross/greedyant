3a976ac88496da51da09f583648bed3dcf1187a2
Faktor 10 <oreillyross@gmail.com>
2020-08-26 09:34:50 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Container from "./components/Container";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
 
       <Container />
+      <Footer/>
     </div>
   );
 }
