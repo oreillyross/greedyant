4612987493d252511dba8c66744abe788e51246b
Faktor 10 <oreillyross@gmail.com>
2020-08-26 15:43:14 +0200

    from Master (#5)
    * align with codesandbox (#4)
    
    * merge from codesandbox (#3)
    
    * add
    
    * add
    
    * update to allow create-react-app functionality
    
    * app renders with dependencies installed
    
    * style Header
    
    * style change
    
    * refactor Header into seperate component
    
    * add react-testing library
    
    * add Container with styling
    
    * add eslint
    
    * add contnet box
    
    * add footer
    
    * add skip preflight
    
    * add svg icon

---
+++ src/components/AddBox.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./AddBox.module.css";
+
+export default function AddBox(props) {
+  return <div className={styles.AddBox}>
+    <img src='plus-circle.svg' alt='add item'/>
+  </div>;
+}
