4cdd150a660a047e60bdc5e819248df657b61eed
Faktor 10 <oreillyross@gmail.com>
2020-08-26 15:43:14 +0200

    from Master (#5)
    * align with codesandbox (#4)
    
    * merge from codesandbox (#3)
    
    * add
    
    * add
    
    * update to allow create-react-app functionality
    
    * app renders with dependencies installed
    
    * style Header
    
    * style change
    
    * refactor Header into seperate component
    
    * add react-testing library
    
    * add Container with styling
    
    * add eslint
    
    * add contnet box
    
    * add footer
    
    * add skip preflight
    
    * add svg icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Header from './components/Header'n
-import Container from './components/Container'
+
+import Header from "./components/Header";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+
 
 export default function App() {
   return (
     <div>
-      <Header/>
+      <Header />
+
+
+      <Container />
+      <Footer/>
 
       <Container/>
+
 
 
 
