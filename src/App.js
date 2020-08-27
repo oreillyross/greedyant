3bc41fd3ea45c3bbddcfcdf43a8ac05f4a627142
Ross O'Reilly <oreillyross@gmail.com>
2020-08-27 21:31:23 +0200

    add demoData

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Header from "./components/Header";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 
+
+const demoData = [{ name: "Bob" }, { name: "Jimmy" }, { name: "Jones" }]
+
 export default function App() {
   return (
     <div>
       <Header />
-      <Container />
+      <Container contents={demoData}/>
       <Footer/>
     </div>
   );
