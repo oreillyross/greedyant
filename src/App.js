35db52c51de94587ae1c5b3572d2834fa12f71cf
Ross O'Reilly <oreillyross@gmail.com>
2020-08-27 19:56:51 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Container from './components/Container';
+import Footer from './components/Footer'
 
 export default function App() {
   return (
