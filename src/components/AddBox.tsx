24cd21291ab05e2c044e9db5bb3e6a5ce350eb76
Ross O'Reilly <oreillyross@gmail.com>
2020-08-29 17:36:43 +0200

    stuff

--- src/components/AddBox.tsx
+++ src/components/AddBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./AddBox.module.css";
 
-export default function AddBox(props) {
+export default function AddBox(props: {}) {
   return <div className={styles.AddBox}>
     <img src='plus-circle.svg' alt='add item'/>
   </div>;
