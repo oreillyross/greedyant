19b9b1ecad22590431f176215513cbcc14382f5c
Ross O'Reilly <oreillyross@gmail.com>
2020-08-30 19:20:27 +0200

    moving to typescript

--- src/components/AddBox.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./AddBox.module.css";
-
-export default function AddBox(props) {
-  return <div className={styles.AddBox}>
-    <img src='plus-circle.svg' alt='add item'/>
-  </div>;
-}
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,8 +5,14 @@ import styles from "./Container.module.css";
 import ContentBox from "./ContentBox";
 import AddBox from './AddBox'
 
+type ContentProps = {
+  content: {
+    name: string
+  }
+}
+
 // (array[{}]?) => AddBox ? ContentBox[]
-export default function Container({content}) {
+export default function Container({content}: ContentProps) {
   return (
     <div className={styles.container}>
      {(!content) ? <AddBox/> : null}
