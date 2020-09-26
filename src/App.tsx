a6815f1b8c7faa27e2d01238d6bf40812abd6f80
Ross O'Reilly <oreillyross@gmail.com>
2020-09-26 15:33:52 +0200

    readd Container

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import Header from "./components/Header";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 
-const demoData = [{ name: "Bob" }, { name: "Jimmy" }, { name: "Jones" }];
-
 export default function App() {
   return (
     <div>
       <Header />
-      <Container />
+        <Container/>
       <Footer/>
     </div>
   );
