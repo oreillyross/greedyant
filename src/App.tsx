fb00145715b902d2080bea69d974a4b505535f3b
Ross O'Reilly <oreillyross@gmail.com>
2020-10-06 19:04:00 +0200

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import Header from "./components/Header";
 import Container from "./components/Container";
-import Footer from "./components/Footer";
 
 export default function App() {
   return (
