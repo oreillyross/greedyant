f8cc333b915f492f5b161dd205d1f9e5fe40bbe0
Ross O'Reilly <oreillyross@gmail.com>
2020-10-12 16:03:54 +0200

    styling

--- src/components/IngredientCard.tsx
+++ src/components/IngredientCard.tsx
@@ -2,23 +2,52 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 const Card = styled.div`
-   width: 100px;
-   height: 100px;
+   padding: 1.2rem;
    border: 1px solid black;
+   width: 200px;
 `
+
+const Container = styled.div`
+  padding: 12px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 3px;
+`
+
 function IngredientCard() {
     return (
-        <div>
+        <Container>
             
             <Card>
             <header><h3>Turmeric</h3></header>
             <pre>Verstegen</pre>
             amount left: 45g
             original amount: 49g
-            expiry date: 15/06/2023
+            <div>expiry date: 15/06/2023</div>
+            </Card>
+            <Card>
+            <header><h3>Turmeric</h3></header>
+            <pre>Verstegen</pre>
+            amount left: 45g
+            original amount: 49g
+            <div>expiry date: 15/06/2023</div>
+            </Card>
+            <Card>
+            <header><h3>Turmeric</h3></header>
+            <pre>Verstegen</pre>
+            amount left: 45g
+            original amount: 49g
+            <div>expiry date: 15/06/2023</div>
+            </Card>
+            <Card>
+            <header><h3>Turmeric</h3></header>
+            <pre>Verstegen</pre>
+            amount left: 45g
+            original amount: 49g
+            <div>expiry date: 15/06/2023</div>
             </Card>
         
-        </div>
+        </Container>
     )
 }
 
