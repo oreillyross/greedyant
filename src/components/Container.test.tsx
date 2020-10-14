93729a1a64912aae9f54b2419ad44763ed422e77
Ross O'Reilly <oreillyross@gmail.com>
2020-10-14 19:21:09 +0200

    start with testing

--- src/components/Container.test.tsx
+++ src/components/Container.test.tsx
@@ -10,17 +10,6 @@ describe('Container', () => {
 it('renders a Container', () => {
   render(<Container content={[{name: 'bob'}]}/>);
 })
-it('renders AddBox if no array passed', () => {
-render(<Container/>)
 
-const addBox = screen.getByAltText('add item', {exact: true})
-expect(addBox).toBeInTheDocument()
-
-})
- 
-it('renders 3 ContentBoxes if array with three objects passed', () => {
-  render(<Container contents={demoData}/>)
-  expect(screen.getAllByTestId('content-box').length).toEqual(3)
-})
 })
 
