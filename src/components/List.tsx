17e6337a6dbba134daf404424b7151f7027a6fbd
Ross O'Reilly <oreillyross@gmail.com>
2020-11-01 17:18:09 +0100

    adding List

---
+++ src/components/List.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+
+const Outline = styled.div`
+  margin: 1em;
+  padding: 1em;
+  border: 1px dashed red;
+
+`
+
+
+
+const List = () => {
+    return <Outline>A generic list</Outline>
+}
+
+export { List }
