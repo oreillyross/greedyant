9a4d2724edc275983b88a6c5ca96d33060abf15e
Ross O'Reilly <oreillyross@gmail.com>
2020-11-03 14:35:17 +0100

    styled list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,18 +1,55 @@
-import * as React from 'react'
-import styled from 'styled-components'
+import * as React from "react";
+import styled from "styled-components";
+import data from "../db.json";
 
+console.log(data);
 
 const Outline = styled.div`
   margin: 1em;
   padding: 1em;
   border: 1px dashed red;
+`;
 
+const StyledTable = styled.table`
+   width: 100%;
+   & > tr {
+     background: red;
+   }
 `
 
-
-
-const List = () => {
-    return <Outline>A generic list</Outline>
+interface Ingredient {
+  quantity: number;
+  measurement: string;
+  name: string;
+  expiryDate: Date;
 }
 
-export { List }
+type Ingredients = Ingredient[];
+
+const List = () => {
+  return (
+    <Outline>
+      <StyledTable>
+        <tr>
+          <th>Quantity</th>
+          <th>Measurement</th>
+          <th>Name</th>
+          <th>Expiry date</th>
+        </tr>
+        {data.ingredients.map((ingredient) => {
+          return (
+            <tr>
+              <td>{ingredient.quantity}</td>
+
+              <td>{ingredient.measurement}</td>
+              <td>{ingredient.name}</td>
+              <td>{ingredient.expiryDate}</td>
+            </tr>
+          );
+        })}
+      </StyledTable>
+    </Outline>
+  );
+};
+
+export { List };
