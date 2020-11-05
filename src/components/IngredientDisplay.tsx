7a0630d5d0449598f9b96f75aa8c60438c26f3f4
Ross O'Reilly <oreillyross@gmail.com>
2020-11-05 11:51:05 +0100

    style

--- src/components/IngredientDisplay.tsx
+++ src/components/IngredientDisplay.tsx
@@ -1,32 +1,33 @@
-import * as React from 'react'
-import styled from 'styled-components'
-
+import * as React from "react";
+import styled from "styled-components";
 
 //ups roca
 // url placement size repeat o clip attachment
 const StyledDisplay = styled.div`
+  position: relative;
   margin: 24px;
   font-size: 16px;
   padding: 16px;
   width: 400px;
   height: 400px;
   border: 1px solid green;
-  background-image: url('https://res.cloudinary.com/pantler/image/upload/c_scale,w_384/v1556653564/1200px-Bright_red_tomato_and_cross_section02_u8hklz.jpg');
+  background-image: url("https://res.cloudinary.com/pantler/image/upload/c_scale,w_384/v1556653564/1200px-Bright_red_tomato_and_cross_section02_u8hklz.jpg");
   background-repeat: no-repeat;
   background-size: contain;
   border-radius: 20px;
-  
-`
+`;
+
+const StyledName = styled.div`
+  position: absolute;
+  top: 25px;
+`;
 
 const IngredientDisplay = () => {
-    return (
+  return (
+    <StyledDisplay>
+      <StyledName>Tomatoes</StyledName>
+    </StyledDisplay>
+  );
+};
 
-        <StyledDisplay>
-            <div>
-                Tomatoes
-            </div>
-        </StyledDisplay>
-    )
-}
-
-export { IngredientDisplay }
+export { IngredientDisplay };
