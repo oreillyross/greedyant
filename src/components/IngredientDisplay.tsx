2fef528a83e20ea66e7100d7b9bcf735e281dae7
Ross O'Reilly <oreillyross@gmail.com>
2020-11-05 09:02:28 +0100

    add

---
+++ src/components/IngredientDisplay.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+
+//ups roca
+// url placement size repeat o clip attachment
+const StyledDisplay = styled.div`
+  margin: 24px;
+  padding: 16px;
+  width: 400px;
+  height: 400px;
+  border: 1px solid green;
+  background-image: url('https://res.cloudinary.com/pantler/image/upload/c_scale,w_384/v1556653564/1200px-Bright_red_tomato_and_cross_section02_u8hklz.jpg');
+  background-repeat: no-repeat;
+  background-size: contain;
+`
+
+const IngredientDisplay = () => {
+    return (
+
+        <StyledDisplay>
+            <div>
+                Tomatoes
+            </div>
+        </StyledDisplay>
+    )
+}
+
+export { IngredientDisplay }
