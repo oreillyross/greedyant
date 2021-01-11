784861bf454e9b8de00293a7e0571c5ddf0e6486
Faktor 10 <oreillyross@gmail.com>
2021-01-11 15:15:09 +0100

    fixed error

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,7 @@
 import React from "react";
 import styles from "./Container.module.css";
-import ContentBox from "./ContentBox";
-import AddBox from './AddBox'
-
-
 
 // (array[{}]?) => AddBox ? ContentBox[]
-export default function Container({content}) {
-  console.log(content.length)
-  return (
-    <div className={styles.container}>
-     {(content.length === 0) ? <AddBox/> : null}
-    </div>
-  );
+export default function Container({ content }) {
+  return <div className={styles.container}></div>;
 }
