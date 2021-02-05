0f2c8223c071607c5fb1ab967c8733583f127fc7
Ross O'Reilly <oreillyross@gmail.com>
2021-02-05 16:52:46 +0100

    add

---
+++ src/components/IngredientStatusBar.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import '../styles/IngredientStatusBar.css';
+
+const IngredientStatusBar = () => {
+    return (
+        <div>
+           Hello from Ingredient Status Bar
+        </div>
+    )
+}
