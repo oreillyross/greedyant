aaee78a8db12def764403c79487d4fb7fc5cd6bd
Faktor 10 <oreillyross@gmail.com>
2021-02-17 16:49:47 +0100

    Update schema.js

--- server/src/schema.js
+++ server/src/schema.js
@@ -18,3 +18,21 @@ const typeDefs = gql`
   }
 
 `
+
+const ingredients = [
+  {
+    id: 1,
+    name: 'Black Pepper',
+    quantity: 12.5 
+  },
+  {
+    id: 2,
+    name: 'White onions',
+    quantity: 3
+  },
+  {
+    id: 3,
+    name: 'Mustard',
+    quantity: 230 
+  }
+]
