f2cc2af48a3cd3d5182d39c65fd7bc4c7a7d9b2d
Ross O'Reilly <oreillyross@gmail.com>
2021-03-05 16:54:53 +0100

    setup ApolloClient, move deps to devdeps

--- server/index.ts
+++ server/index.ts
@@ -11,12 +11,31 @@ const books = [
     },
   ];
 
+  const ingredients = [
+
+    { 
+        name: 'pepper',
+        quantity: 50,
+        measurement: 'g'
+
+    },
+    { 
+        name: 'potatoes',
+        quantity: 1,
+        measurement: 'kg'
+
+    }
+    
+
+  ]
+
 
   // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
       books: () => books,
+      ingredients: () => ingredients
     },
   };
 
@@ -29,11 +48,18 @@ const typeDefs = gql`
     author: String
   }
 
+  type Ingredient {
+      name: String
+      quantity: Int
+      measurement: String
+  }
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+    ingredients: [Ingredient]
   }
 `
 
--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import { IngredientForm } from "./components/IngredientForm";
 import { Header } from "./components/Header";
 import styled from "styled-components";
 import { IngredientDisplay } from "./components/IngredientDisplay";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-
+const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(),
+});
 
 const StyledStrap = styled.div`
   font-size: 16px;
@@ -24,11 +27,11 @@ const StyledApp = styled.div`
 `;
 
 const StyledBody = styled.section`
-min-height: 100vh;
-display: flex;
-align-items: center;
-justify-content: center;  
-`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function App() {
   return (
@@ -37,8 +40,8 @@ export default function App() {
         <Logo /> <Header />
         <StyledStrap>The app that makes cooking what you got in the pantry a breeze.</StyledStrap>
       </StyledApp>
-        <IngredientForm/>
-        <IngredientDisplay ingredients={[]}/>
+      <IngredientForm />
+      <IngredientDisplay ingredients={[]} />
     </>
   );
 }
